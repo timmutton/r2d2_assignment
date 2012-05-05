e1a03902fbe7bafecdb3c81d5172c5d2d7a5a172
Jakub Stasiak <kuba.stasiak@gmail.com>
2012-05-05 22:17:41 +1000

    Use DTW library in GestureRecognizer

--- unity_project/Assets/Gesture Recognition/GestureRecognizer.cs
+++ unity_project/Assets/Gesture Recognition/GestureRecognizer.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using DTW;
 using UnityEngine;
 using System.Collections;
 using WiimoteLib;
@@ -14,11 +15,11 @@ public enum Gestures : int
 }
 
 public struct GestureMatch {
-	public float matchValue;
+	public float distance;
 	public string name;
 }
 
-public class GestureRecognizer : MonoBehaviour {
+public class GestureRecognizer  {
 	public Shader shader;
 	Vector3 currentPosition;
 	Vector3 initialPosition;
@@ -35,16 +36,69 @@ public class GestureRecognizer : MonoBehaviour {
 	private static Dictionary<Vector2[], string> data = new Dictionary<Vector2[], string> {
 		{ new[] { new Vector2(-1, 0) }, "hline"},
         { new[] { new Vector2(1, 0) }, "hline"},
+
 		{ new[] { new Vector2(0, -1) }, "vline"},
-		{ new[] { new Vector2(0, 1) }, "vline"},                                                                             	
+		{ new[] { new Vector2(0, 1) }, "vline"},   
+        
+        { new[] { new Vector2(1, 0), new Vector2(-1, -1), new Vector2(1, 0) }, "zet"},
+
+		{ new[] { new Vector2(1, -1), new Vector2(1, 1) }, "vdown" }, 
+		{ new[] { new Vector2(1, -1.5f), new Vector2(1, 1.5f) }, "vdown" }, 
+		{ new[] { new Vector2(1, -2f), new Vector2(1, 2.0f) }, "vdown" }, 
+
+		{ new[] { new Vector2(1.5f, -1), new Vector2(1.5f, 1) }, "vdown" }, 
+		{ new[] { new Vector2(2.0f, -1), new Vector2(2.0f, 1) }, "vdown" }, 
+
+
+		{ new[] { new Vector2(1, 1), new Vector2(1, -1) }, "vup" }, 
+		{ new[] { new Vector2(1, 1.5f), new Vector2(1, -1.5f) }, "vup" }, 
+		{ new[] { new Vector2(1, 2f), new Vector2(1, -2.0f) }, "vup" }, 
+
+		{ new[] { new Vector2(1.5f, 1), new Vector2(1.5f, -1) }, "vup" }, 
+		{ new[] { new Vector2(2.0f, 1), new Vector2(2.0f, -1) }, "vup" }, 
 	};
 
+
 	public static string recognizeGesture(List<Vector2> normalizedAccelerations) {
-		throw new GestureNotFoundException();
+		Debug.Log("accelerations:");
+		for(int i = 0; i < normalizedAccelerations.Count; ++i) {
+			Debug.Log(string.Format("{0}: {1}", i, normalizedAccelerations[i]));
+		}
+
+		var arr = normalizedAccelerations.ToArray();
+		var matches = getMatches(arr);
+
+		var threshold = 6f;
+		Debug.Log("matches (with distance < threshold):");
+		foreach (var m in matches.Where(e => e.distance < threshold)) {
+			Debug.Log(string.Format("{0} (distance: {1})", m.name, m.distance));
+		}
+
+		string gesture;
+		if (matches.Count() == 0 || matches.First().distance > threshold) {
+			throw new GestureNotFoundException();
+		}
+		else {
+			var fm = matches.First();
+			Debug.Log(string.Format("Successfully matched gesture: {0} (distance: {1})", fm.name, fm.distance));
+			gesture = fm.name;
+		}
+
+		return gesture;
 	}
 
-	public static void saveGesture(string name, List<Vector2> normalizedAccelerations) {
-		data.Add(normalizedAccelerations.ToArray(), name);
+	private static IOrderedEnumerable<GestureMatch> getMatches(Vector2[] accelerations)
+	{
+		var list = new List<GestureMatch>();
+		foreach (var kv in data)
+		{
+			float xmatch = SimpleDTW.get(accelerations.Select(e => e.x).ToArray(), kv.Key.Select(e => e.x).ToArray());
+			float ymatch = SimpleDTW.get(accelerations.Select(e => e.y).ToArray(), kv.Key.Select(e => e.y).ToArray());
+			list.Add(new GestureMatch { name = kv.Value, distance = new Vector2(xmatch, ymatch).SqrMagnitude() });
+		}
+
+		var sorted = list.OrderBy(el => el.distance);
+		return sorted;
 	}
 	
 	public static List<Vector2> getAccelerationsFromPoints(ArrayList points) {
@@ -70,7 +124,7 @@ public class GestureRecognizer : MonoBehaviour {
 	}
 
 	private static List<Vector2> filterAccelerationsByMagnitude(List<Vector2> accelerations) {
-		return accelerations.Where(v => Vector3.Magnitude(v) > 1).ToList();
+		return accelerations.Where(v => Vector3.Magnitude(v) > 5).ToList();
 	}
 
 	private static List<Vector2> filterAccelerationsByDirection(List<Vector2> accelerations) {
