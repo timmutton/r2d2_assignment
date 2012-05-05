00296f89d6e42227a9a57aa38ca0518deb6607fc
Karim Ainine <karim.ainine@gmail.com>
2012-05-05 23:49:41 +1000

    HMM Library files

---
+++ unity_project/Assets/Gesture Recognition/HMMRecognizer.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class HMMRecognizer
+{
+	private int[][] sequences = new int[][] 
+	{
+	    new int[] { 0,1,1,1,1,1,1 },
+	    new int[] { 0,1,1,1 },
+	    new int[] { 0,1,1,1,1 },
+	    new int[] { 0,1, },
+	    new int[] { 0,1,1 },
+	};	
+}
+
