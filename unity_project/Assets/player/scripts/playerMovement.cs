eee6b264803fe6a8d1b9f435bfad5e3c48d489a8
blu3jack <timothy.mutton@gmail.com>
2012-05-06 11:19:03 +1000

    Swapped around controls and added jumping

--- unity_project/Assets/player/scripts/playerMovement.cs
+++ unity_project/Assets/player/scripts/playerMovement.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 using System.Collections;
 
 public class playerMovement : MonoBehaviour {
-	public float rotationSpeed = 1f, movementSpeed = 1.0f, gravityForce = 20.0f;
+	public float rotationSpeed = 1f, movementSpeed = 6.0f, jumpSpeed = 8.0f, gravityForce = 20.0f;
 	public bool useKeyboard = false, invertXAxes = false;
 	
 	private ClientWiiState state;
 	private Transform playerCam;
 	private CharacterController controller;
-	private Vector3 moveDir, rotDir;
+	private Vector3 moveDir = Vector3.zero, rotDir = Vector3.zero;
 	
 	void Start(){
 		state = new ClientWiiState();
@@ -16,29 +16,48 @@ public class playerMovement : MonoBehaviour {
 		controller = GetComponent<CharacterController>();
 	}
 
-	void FixedUpdate(){
-		if(useKeyboard){
-			moveDir = new Vector3(Input.GetAxis("moveX"), 0, Input.GetAxis("moveZ"));
-			rotDir = new Vector3(Input.GetAxis("lookX"), Input.GetAxis("lookY"), 0);
-		}else{
-			rotDir = new Vector3(state.ncJoyY, state.ncJoyX, 0);
-			moveDir = new Vector3(0, 0, 0);
-			if(!state.B){
+	void Update(){
+		if(controller.isGrounded){
+			if(useKeyboard){				
+				rotDir = new Vector3(Input.GetAxis("lookX"), Input.GetAxis("lookY"), 0);
+				moveDir = new Vector3(Input.GetAxis("moveX"), 0, Input.GetAxis("moveZ"));
+				moveDir = transform.TransformDirection(moveDir) * CurrentMovementSpeed();
+				
+				if(Input.GetButton("jump")){
+					print("jump");
+					moveDir.y = jumpSpeed;
+				}
+			}else{
+				rotDir = new Vector3(state.ncJoyY, state.ncJoyX, 0);
+				moveDir = new Vector3(0, 0, 0);
 				if(state.Left)
 					moveDir.x = -1.0f;
 				if(state.Right)
 					moveDir.x = 1.0f;
-				
+					
 				if(state.Down)
 					moveDir.z = -1.0f;
 				if(state.Up)
 					moveDir.z = 1.0f;
+				
+				moveDir = transform.TransformDirection(moveDir) * CurrentMovementSpeed();
+				
+				if(state.B){
+					print("jump");
+					moveDir.y = jumpSpeed;
+				}
 			}
+		}else{
+			moveDir.y -= gravityForce * Time.deltaTime;
 		}
 		
-		moveDir = transform.TransformDirection(moveDir) * CurrentMovementSpeed();
-		moveDir.y = controller.velocity.y - gravityForce * Time.deltaTime;
 		controller.Move(moveDir * Time.deltaTime);
+		
+		if(useKeyboard){				
+			rotDir = new Vector3(Input.GetAxis("lookX"), Input.GetAxis("lookY"), 0);
+		}else{
+			rotDir = new Vector3(state.ncJoyY, state.ncJoyX, 0);	
+		}
 		
 		if(!invertXAxes){
 			rotDir.x *= -1.0f;
