6b1bd49189fd44a1dcc3bb31c9eb0ccf0ca177f8
Karim Ainine <karim.ainine@gmail.com>
2012-05-06 16:21:52 +1000

    HMM Testing

--- unity_project/Assets/Gesture Recognition/HMMRecognizer.cs
+++ unity_project/Assets/Gesture Recognition/HMMRecognizer.cs
@@ -1,6 +1,11 @@
 using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using DTW;
 
-public class HMMRecognizer
+public class HMMRecognizer : MonoBehaviour
 {
 	private int[][] sequences = new int[][] 
 	{
@@ -10,5 +15,60 @@ public class HMMRecognizer
 	    new int[] { 0,1, },
 	    new int[] { 0,1,1 },
 	};	
+	
+	Vector3 currentPosition;
+	Vector3 initialPosition;
+	Event currentEvent;
+	ArrayList points = new ArrayList();
+	
+	void OnGUI() {
+        currentEvent = Event.current;
+    }
+
+	void Update(){
+		if(currentEvent.type == EventType.mouseDown){
+			initialPosition = Input.mousePosition;
+			points.Clear();
+			points.Add(initialPosition);
+		}else if(currentEvent.type == EventType.mouseDrag){
+			currentPosition = Input.mousePosition;
+			points.Add(currentPosition);
+		}else if(currentEvent.type == EventType.MouseUp){
+			points.Add(currentPosition);
+			Debug.Log("Before filtering:\n" + printVectors(points));
+			
+			Debug.Log("Directions:\n" + printVectors2(GestureRecognizer.getAccelerationsFromPoints(points)));
+			try{
+			}catch(GestureNotFoundException e){
+				Debug.Log("" + e.Message);
+			}
+		}
+	}
+	
+	string printVectors(ArrayList points){
+		string result = "";
+		for(int i=0; i<points.Count; i++){
+			Vector3 point = (Vector3) points[i];
+			if(i==0){
+				result = "(" + point.x + "," + point.y + ")";
+			}else{
+				result += " - (" + point.x + "," + point.y + ")";
+			}
+		}
+		return result;
+	}       
+	
+	string printVectors2(List<Vector2> points){
+		string result = "";
+		for(int i=0; i<points.Count; i++){
+			Vector2 point = points[i];
+			if(i==0){
+				result = "(" + point.x + "," + point.y + ")";
+			}else{
+				result += " - (" + point.x + "," + point.y + ")";
+			}
+		}
+		return result;
+	}       
 }
 
