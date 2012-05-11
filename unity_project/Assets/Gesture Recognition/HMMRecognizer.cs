6b7f30a59d3b81038a03bd05aa548b2154903ccd
Karim Ainine <karim.ainine@gmail.com>
2012-05-11 14:26:04 +1000

    HMM Recognizer working

--- unity_project/Assets/Gesture Recognition/HMMRecognizer.cs
+++ unity_project/Assets/Gesture Recognition/HMMRecognizer.cs
@@ -15,102 +15,243 @@ public class HMMRecognizer : MonoBehaviour
 	public const int SOUTH_EAST = 6;
 	public const int SOUTH_WEST = 7;
 	
-	private int[][] square_sequences = new int[][] 
-	{
-	    new int[]{ NORTH, EAST, SOUTH, WEST},
-		new int[] { EAST, SOUTH, WEST, NORTH},
-		new int[] { SOUTH, WEST, NORTH, EAST},
-	    new int[] { WEST, NORTH, EAST, SOUTH}
-	};
+	public const int SQUARE = 8;
+	public const int V_DOWN = 9;
+	public const int V_UP = 10;
+	public const int H_LINE = 11;
+	public const int V_LINE = 12;
 	
-	double [,] A = new double[4,4] 
-	{
-		{0.5,0.5,0,0},
-		{0,0.5,0,0},
-		{0,0,0.5,0},
-		{0,0,0.5,0.5}
-	};
-	
-	double [,] B = new double[4,4]
-	{
-		{0.5,0.5,0,0},
-		{0,0.5,0,0},
-		{0,0,0.5,0},
-		{0,0,0.5,0.5}
-	};
-	
-	double [] pi = new double [] {0.5, 0.5, 0.0, 0.0};
-	
-	Vector3 currentPosition;
-	Vector3 initialPosition;
-	Event currentEvent;
-	ArrayList points = new ArrayList();
-
-	void testHmm(){
+	double squareEvalution(int [] input){
+		int[][] sequences = new int[][] 
+		{
+		    new int[]{ NORTH, EAST, SOUTH, WEST},
+			new int[] { EAST, SOUTH, WEST, NORTH},
+			new int[] { SOUTH, WEST, NORTH, EAST},
+		    new int[] { WEST, NORTH, EAST, SOUTH}
+		};
+		
+		double [,] A = new double[8,8] 
+		{
+			{0, 0, 1, 0, 0, 0, 0, 0},
+			{0, 0, 0, 1, 0, 0, 0, 0},
+			{0, 1, 0, 0, 0, 0, 0, 0},
+			{1, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0}
+		};
+		
+		double [,] B = new double[8,8]
+		{
+			{0.25, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0.25, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0.25, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0.25, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0}
+		};
+		
+		double [] pi = new double [] {0.25,0.25,0.25,0.25, 0, 0, 0, 0};
+		
 		HiddenMarkovModel model = new HiddenMarkovModel(A, B, pi);
-		model.Learn(square_sequences, 4, 0.0001);
+		model.Learn(sequences, 4, 0.0001);
 		
-		Debug.Log("\nEvaluation 1: " + model.Evaluate(new int[] {NORTH, EAST, SOUTH, WEST}));
+		return model.Evaluate(input);
+	}
+	
+	double vDownEvalution(int [] input){
 		
-		Debug.Log("\nEvaluation 2: " + model.Evaluate(new int[] { NORTH, EAST, WEST, SOUTH}));
+		int[][] sequences = new int[][] 
+		{
+		    new int[]{ SOUTH_EAST, NORTH_EAST},
+			new int[] { SOUTH_WEST, NORTH_WEST}
+		};
 		
-		Debug.Log("\nEvaluation 3: " + model.Evaluate(new int[] { NORTH, SOUTH, EAST, WEST}));
+		double [,] A = new double[8,8] 
+		{
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 1, 0, 0, 0},
+			{0, 0, 0, 0, 0, 1, 0, 0}
+		};
 		
-		Debug.Log("\nEvaluation 4: " + model.Evaluate(new int[] { EAST, SOUTH, WEST, NORTH}));
+		double [,] B = new double[8,8]
+		{
+			{1, 0, 0, 0, 0, 0, 0, 0},
+			{0, 1, 0, 0, 0, 0, 0, 0},
+			{0, 0, 1, 0, 0, 0, 0, 0},
+			{0, 0, 0, 1, 0, 0, 0, 0},
+			{0, 0, 0, 0, 1, 0, 0, 0},
+			{0, 0, 0, 0, 0, 1, 0, 0},
+			{0, 0, 0, 0, 0, 0, 1, 0},
+			{0, 0, 0, 0, 0, 0, 0, 1}
+		};
+		
+		double [] pi = new double [] {0, 0, 0, 0, 0, 0, 0.5, 0.5};
+		
+		HiddenMarkovModel model = new HiddenMarkovModel(A, B, pi);
+		model.Learn(sequences, 0.0001);
+		
+		return model.Evaluate(input);
+	}
+	
+	double vUpEvalution(int [] input){
+		
+		int[][] sequences = new int[][] 
+		{
+		    new int[]{ NORTH_EAST, SOUTH_EAST},
+			new int[] { NORTH_WEST, SOUTH_WEST}
+		};
+		
+		double [,] A = new double[8,8] 
+		{
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 1, 0},
+			{0, 0, 0, 0, 0, 0, 0, 1},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0}
+		};
+		
+		double [,] B = new double[8,8]
+		{
+			{1, 0, 0, 0, 0, 0, 0, 0},
+			{0, 1, 0, 0, 0, 0, 0, 0},
+			{0, 0, 1, 0, 0, 0, 0, 0},
+			{0, 0, 0, 1, 0, 0, 0, 0},
+			{0, 0, 0, 0, 1, 0, 0, 0},
+			{0, 0, 0, 0, 0, 1, 0, 0},
+			{0, 0, 0, 0, 0, 0, 1, 0},
+			{0, 0, 0, 0, 0, 0, 0, 1}
+		};
+		
+		double [] pi = new double [] {0, 0, 0, 0, 0.5, 0.5, 0, 0};
+		
+		HiddenMarkovModel model = new HiddenMarkovModel(A, B, pi);
+		model.Learn(sequences, 0.0001);
+		
+		return model.Evaluate(input);
+	}
+	
+	double verticalEvalution(int [] input){
+		
+		int[][] sequences = new int[][] 
+		{
+		    new int[]{ NORTH },
+			new int[] { SOUTH }
+		};
+		
+		double [,] A = new double[8,8] 
+		{
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0}
+		};
+		
+		double [,] B = new double[8,8]
+		{
+			{1, 0, 0, 0, 0, 0, 0, 0},
+			{0, 1, 0, 0, 0, 0, 0, 0},
+			{0, 0, 1, 0, 0, 0, 0, 0},
+			{0, 0, 0, 1, 0, 0, 0, 0},
+			{0, 0, 0, 0, 1, 0, 0, 0},
+			{0, 0, 0, 0, 0, 1, 0, 0},
+			{0, 0, 0, 0, 0, 0, 1, 0},
+			{0, 0, 0, 0, 0, 0, 0, 1}
+		};
+		
+		double [] pi = new double [] {0.5, 0.5, 0, 0, 0, 0, 0, 0};
+		
+		HiddenMarkovModel model = new HiddenMarkovModel(A, B, pi);
+		model.Learn(sequences, 0.0001);
+		
+		return model.Evaluate(input);
+	}
+	
+	double horizontalEvalution(int [] input){
+		
+		int[][] sequences = new int[][] 
+		{
+		    new int[]{ EAST },
+			new int[] { WEST }
+		};
+		
+		double [,] A = new double[8,8] 
+		{
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0}
+		};
+		
+		double [,] B = new double[8,8]
+		{
+			{1, 0, 0, 0, 0, 0, 0, 0},
+			{0, 1, 0, 0, 0, 0, 0, 0},
+			{0, 0, 1, 0, 0, 0, 0, 0},
+			{0, 0, 0, 1, 0, 0, 0, 0},
+			{0, 0, 0, 0, 1, 0, 0, 0},
+			{0, 0, 0, 0, 0, 1, 0, 0},
+			{0, 0, 0, 0, 0, 0, 1, 0},
+			{0, 0, 0, 0, 0, 0, 0, 1}
+		};
+		
+		double [] pi = new double [] {0, 0, 0.5, 0.5, 0, 0, 0, 0};
+		
+		HiddenMarkovModel model = new HiddenMarkovModel(A, B, pi);
+		model.Learn(sequences, 0.0001);
+		
+		return model.Evaluate(input);
+	}
+	
+	public int hmmEvalute(int [] input){
+		double square = squareEvalution(input);
+		double v_down = vDownEvalution(input);
+		double v_up = vUpEvalution(input);
+		double horizontal = horizontalEvalution(input);
+		double vertical = verticalEvalution(input);
+		
+		Debug.Log("SQUARE: " + square + "\n");
+		Debug.Log("V_DOWN: " + v_down + "\n");
+		Debug.Log("V_UP: " + v_up + "\n");
+		Debug.Log("Horizontal Line: " + horizontal + "\n");
+		Debug.Log("Vertical Line: " + vertical + "\n");
+		
+		if(vertical >= 0.5){
+			return V_LINE;
+		}else if(horizontal >= 0.5){
+			return H_LINE;
+		}else if(v_up >= 0.5){
+			return V_UP;
+		}else if(v_down >= 0.5){
+			return V_DOWN;
+		}else if(square >= 0.25){
+			return SQUARE;
+		}else{
+			throw new GestureNotFoundException("Gesture not found!");
+		}
 	}
 	
 	void Start(){
-		testHmm();
-	}
-	
-	void OnGUI() {
-        currentEvent = Event.current;
-    }
-
-	void Update(){
-		if(currentEvent.type == EventType.mouseDown){
-			initialPosition = Input.mousePosition;
-			points.Clear();
-			points.Add(initialPosition);
-		}else if(currentEvent.type == EventType.mouseDrag){
-			currentPosition = Input.mousePosition;
-			points.Add(currentPosition);
-		}else if(currentEvent.type == EventType.MouseUp){
-			points.Add(currentPosition);
-			Debug.Log("Before filtering:\n" + printVectors(points));
-			
-			Debug.Log("Directions:\n" + printVectors2(GestureRecognizer.getAccelerationsFromPoints(points)));
-			try{
-			}catch(GestureNotFoundException e){
-				Debug.Log("" + e.Message);
-			}
-		}
-	}
-	
-	string printVectors(ArrayList points){
-		string result = "";
-		for(int i=0; i<points.Count; i++){
-			Vector3 point = (Vector3) points[i];
-			if(i==0){
-				result = "(" + point.x + "," + point.y + ")";
-			}else{
-				result += " - (" + point.x + "," + point.y + ")";
-			}
-		}
-		return result;
-	}       
-	
-	string printVectors2(List<Vector2> points){
-		string result = "";
-		for(int i=0; i<points.Count; i++){
-			Vector2 point = points[i];
-			if(i==0){
-				result = "(" + point.x + "," + point.y + ")";
-			}else{
-				result += " - (" + point.x + "," + point.y + ")";
-			}
-		}
-		return result;
+		Debug.Log("Final Result: " + hmmEvalute(new int[]{ NORTH }) + "\n");
 	}       
 }
 
