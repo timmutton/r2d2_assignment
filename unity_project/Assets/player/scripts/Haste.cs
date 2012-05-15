718218994eb23458dcfcd5231ac7c2ed3db979e0
Jakub Stasiak <kuba.stasiak@gmail.com>
2012-05-15 15:57:20 +1000

    Simple modifier on-screen notification (#9)

--- unity_project/Assets/player/scripts/Haste.cs
+++ unity_project/Assets/player/scripts/Haste.cs
@@ -1,3 +1,11 @@
-﻿class Haste : BaseModifier {
+﻿using System;
+using UnityEngine;
+
+class Haste : BaseModifier {
 	public float MovementSpeedMultiplier { get { return 3.0f; } }
+
+	public override void Start() {
+		base.Start();
+		this.OverlayColor = new Color(1, 1, 1);
+	}
 }
